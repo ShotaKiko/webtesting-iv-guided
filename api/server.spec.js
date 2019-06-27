51d14133a84aa739a2850b1871a64e8b0e897541
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-06-26 21:34:00 -0500

    added testing for get method

---
+++ api/server.spec.js
@@ -0,0 +1,42 @@
+const server = require('./server.js')
+const request = require('supertest')
+
+describe('server.js', () => {
+    xit('should set the test env', ()=> {
+        expect(process.env.DB_ENV).toBe('testing')
+    })
+
+    describe('GET /', () => {
+        it('should return 200', () => {
+            return request(server)
+            .get('/')
+            .then(res => {
+                expect(res.status).toBe(200)
+            })
+        })
+
+        it('should return 200 using async', async () => {
+            const res = await request(server).get('/')
+            expect(res.status).toBe(200)
+        })
+
+        it('should return 200 using async', async () => {
+            const res = await request(server).get('/')
+            expect(res.type).toBe('application/json')
+        })
+
+        it('should return api: up', async () => {
+            const res = await request(server).get('/')
+            expect(res.body).toEqual({ api:'up' })
+        })
+
+        describe('GET/hobbits', () => {
+            it('should return hobbits', async () => {
+                const res = await request(server).get('/hobbits')
+                expect(res.status).toBe(200)
+                expect(res.body).toEqual([])
+            })
+        })
+
+    })
+})
